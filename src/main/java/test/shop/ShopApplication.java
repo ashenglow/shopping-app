be7a441cf97ae48e0d03bbbb40bcfa8e4bbee158
ashenglow <inashenglow@gmail.com>
2024-02-29 16:08:56 +0900

    github action gradle

--- src/main/java/test/shop/ShopApplication.java
+++ src/main/java/test/shop/ShopApplication.java
@@ -1,11 +1,32 @@
 package test.shop;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@Slf4j
 public class ShopApplication {
+
+    @Value("${server.host.api}")
+    private String host;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+      log.info("host:" + host);
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(host);
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
