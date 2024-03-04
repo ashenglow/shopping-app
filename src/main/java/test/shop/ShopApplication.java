fe7595b8c965cb9e053fdb37094c33d38a25fb7d
ashenglow <inashenglow@gmail.com>
2024-03-04 15:21:51 +0900

    test

--- src/main/java/test/shop/ShopApplication.java
+++ src/main/java/test/shop/ShopApplication.java
@@ -1,13 +1,9 @@
 package test.shop;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 @Slf4j
