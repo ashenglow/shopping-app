8d9cceeca8babd1ad50e4191bf08fd7de9cbbf6f
ashenglow <inashenglow@gmail.com>
2024-03-04 16:39:02 +0900

    test

--- src/main/java/test/shop/ShopApplication.java
+++ src/main/java/test/shop/ShopApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 public class ShopApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
     }
