b645764a294ccdd1f71b5cfd17b7c5905b9b124c
ashenglow <inashenglow@gmail.com>
2024-07-01 15:07:00 +0900

    fix logger to slf4j

--- src/main/java/test/shop/web/auth/filter/MalFormedRequestFilter.java
+++ src/main/java/test/shop/web/auth/filter/MalFormedRequestFilter.java
@@ -4,20 +4,21 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 
 @Component
 public class MalFormedRequestFilter extends OncePerRequestFilter {
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(MalFormedRequestFilter.class);
+    private static final Logger logger = LoggerFactory.getLogger(MalFormedRequestFilter.class);
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String method = request.getMethod();
-         if (method.startsWith("MGLNDD_") || !method.matches("^[A-Z]+$")) {
+        if (method.startsWith("MGLNDD_") || !method.matches("^[A-Z]+$")) {
+            logger.warn("Blocked suspicious request: Method '{}' from IP {}", method, request.getRemoteAddr());
             response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid request method");
             return;
         }
