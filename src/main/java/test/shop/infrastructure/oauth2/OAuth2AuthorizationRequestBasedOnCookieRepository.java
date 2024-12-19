4c37c91e109346a9f686e2550c677ad96751cc53
ashenglow <158246901+ashenglow@users.noreply.github.com>
2024-12-19 15:40:05 +0900

    Update OAuth2AuthorizationRequestBasedOnCookieRepository.java

--- src/main/java/test/shop/infrastructure/oauth2/OAuth2AuthorizationRequestBasedOnCookieRepository.java
+++ src/main/java/test/shop/infrastructure/oauth2/OAuth2AuthorizationRequestBasedOnCookieRepository.java
@@ -2,20 +2,26 @@ package test.shop.infrastructure.oauth2;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.stereotype.Component;
+import test.shop.infrastructure.oauth2.util.CookieUtil;
 
 @Component
+@RequiredArgsConstructor
 public class OAuth2AuthorizationRequestBasedOnCookieRepository implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
 
     public static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
     public static final String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri";
     private static final int cookieExpireSeconds = 180;
+    private final CookieUtil cookieUtil;
 
     @Override
     public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
-        return null;
+        CookieUtil.getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME)
+
+
     }
 
     @Override
