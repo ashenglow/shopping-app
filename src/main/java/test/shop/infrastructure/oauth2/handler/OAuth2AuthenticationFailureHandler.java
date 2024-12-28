2d0693d127005bf5d3fbb320e473d0a7ec33c6a5
ashenglow <158246901+ashenglow@users.noreply.github.com>
2024-12-28 18:18:33 +0900

    Dev (#40)
    * Updated submodule in dev branch
    
    * modify ddl-auto
    
    * Feature/test jmeter (#22)
    
    * make perform test, add test profile
    
    * add SpecificationBuilderV2
    
    * add permit for pay
    
    * Delete DbInit.java
    
    * Update SpecificationBuilderV2.java (#24)
    
    fix getList err when ratings=0
    
    * Feature/fix cartcont (#26)
    
    * merge dev into main (#25)
    
    * Updated submodule in dev branch
    
    * modify ddl-auto
    
    * Feature/test jmeter (#22)
    
    * make perform test, add test profile
    
    * add SpecificationBuilderV2
    
    * add permit for pay
    
    * Delete DbInit.java
    
    * Update SpecificationBuilderV2.java (#24)
    
    fix getList err when ratings=0
    
    * Update AOP settings in submodule
    
    * Feature/test jmeter (#22)
    
    * make perform test, add test profile
    
    * add SpecificationBuilderV2
    
    * add permit for pay
    
    * fix multiple delete
    
    * Restructuring Project Folders for Cleaner Organization
    
    * Update submodule to use AOP configuration from main
    
    * add monitoring
    
    * arrange Entity structure
    
    add Image sample to Item
    arrange DTO, service, controller
    refactor Init DB
    
    * add InitController
    
    * Feature/sync review (#29)
    
    * fix review methods
    
    add reference to Review & Item entity
    separate review controller from item's
    add method synchronizing review & member & item entity
    add method calculating ratings & num of reviews
    
    * Synchronizing Item Review Count and Ratings
    
    add reference valid items and members to review
    add calculation for Review counts and ratings
    
    * Update sub
    
    * Feature/enhance monitor (#31)
    
    * enhance monitoring order flow metrics
    
    add query for order-specific metrics
    enhance the visualization
    
    * enhance monitoring
    
    * enhance monitoring dashboard
    
    enhance the monitoring dashboard to handle all types of monitoring data
    
    * Feature/cg extractid (#33)
    
    * remove pathVariable while auth
    
    * add select all and clear cart
    
    * modify remove cartItems
    
    * Update submodule reference to match main branch
    
    * Feature/add oauth2 (#38)
    
    * Update JwtAuthFilter.java
    
    * Modify member entity fields
    
    * Add new columns to DB
    
    * Add OAuth2
    
    * Update OAuth2AuthorizationRequestBasedOnCookieRepository.java
    
    * Update OAuth2AuthorizationRequestBasedOnCookieRepository.java
    
    * Update OAuth2AuthenticationSuccessHandler.java
    
    * Update OAuth2AuthenticationSuccessHandler.java
    
    * add google oAuth2
    
    * fix frontendUrl

--- src/main/java/test/shop/infrastructure/oauth2/handler/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/test/shop/infrastructure/oauth2/handler/OAuth2AuthenticationFailureHandler.java
@@ -3,6 +3,7 @@ package test.shop.infrastructure.oauth2.handler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -11,11 +12,13 @@ import java.io.IOException;
 
 @Component
 public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                         AuthenticationException exception) throws IOException, ServletException {
         String targetUrl = String.format("%s?error=%s",
-                "http://localhost:3000/oauth2/callback",
+                frontendUrl + "/oauth2/callback",
                 exception.getLocalizedMessage()
         );
 
