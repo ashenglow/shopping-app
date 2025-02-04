0fb4d9e0bce63f8b805b7084f31d5dcb3724a956
ashenglow <158246901+ashenglow@users.noreply.github.com>
2025-02-04 17:32:01 +0900

    fix: fix LazyInitializationException (#84)
    load member.orders using left join fetch

--- src/main/java/test/shop/domain/repository/MemberRepository.java
+++ src/main/java/test/shop/domain/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package test.shop.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import test.shop.domain.model.member.Member;
 
@@ -14,4 +16,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findMemberByUserId(String userId);
     Optional<Member> findMemberById(Long memberId);
     Optional<Member> findMemberByProviderAndProviderId(String provider, String providerId);
+    @Query("SELECT m FROM Member m LEFT JOIN FETCH m.orders WHERE m.id = :memberId")
+    Optional<Member> findByIdWithOrders(@Param("memberId") Long memberId);
 }
